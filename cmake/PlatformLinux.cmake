314c38bb936d0dd22ecb566a6e1bb32cc567018c
Claus Steuer <claus755@gmail.com>
2016-05-26 12:03:16 +0200

    Use clang 3.8 for travis to prevent openmp link errors

--- cmake/PlatformLinux.cmake
+++ cmake/PlatformLinux.cmake
@@ -74,8 +74,6 @@ if(CMAKE_COMPILER_IS_GNUCXX)
 
 elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
 
-    option(LINK_INTEL_OPENMP "When using clang, link intels libiomp5 library instead of GNU's libgomp" ON)
-
     # clang
     set(LINUX_COMPILE_FLAGS
         ${LINUX_COMPILE_FLAGS}
@@ -86,11 +84,7 @@ elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
         -Wno-padded
     )
 
-    if (LINK_INTEL_OPENMP)
-        set(LINUX_LINKER_FLAGS "-fopenmp=libiomp5")
-    else()
-        set(LINUX_LINKER_FLAGS "-fopenmp=libomp")
-    endif()
+    set(LINUX_LINKER_FLAGS "-fopenmp=libomp")
 endif()
 
 
